78b66b71cdb1eba4613f7d838a8c9c28c9f0d96c
Shishir Dey <shishirdey241097@gmail.com>
2024-07-14 22:41:52 +0530

    Update libmodbus-demo examples to support latest libmodbus version

--- examples/Modbus_RTU_Server_MinGW_x86_64/main.cpp
+++ examples/Modbus_RTU_Server_MinGW_x86_64/main.cpp
@@ -4,18 +4,18 @@
 
 int main(void)
 {
-	ModbusRtuServer mRtuServer;
-	mRtuServer.data.writeCoil(0, 1);
-	mRtuServer.data.writeCoil(1, 0);
-	mRtuServer.data.writeCoil(2, 1);
-	mRtuServer.data.writeCoil(3, 0);
-	mRtuServer.data.writeCoil(4, 1);
-	mRtuServer.data.writeCoil(5, 0);
-	mRtuServer.data.writeCoil(6, 1);
-	mRtuServer.data.writeCoil(7, 0);
+	ModbusRtuServer server;
+	server.data.writeCoil(0, 1);
+	server.data.writeCoil(1, 0);
+	server.data.writeCoil(2, 1);
+	server.data.writeCoil(3, 0);
+	server.data.writeCoil(4, 1);
+	server.data.writeCoil(5, 0);
+	server.data.writeCoil(6, 1);
+	server.data.writeCoil(7, 0);
 
 	std::array<uint8_t, 256> request = {6, 1, 0, 0, 0, 8, 0, 0};
-	auto res = mRtuServer.handleRequest(request);
+	auto res = server.process(request);
 
 	std::cout << "[";
 	for (const auto &data : res)
